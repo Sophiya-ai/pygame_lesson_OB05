ba5d9c476780ada2f79f363e77703bd954e4f566
Sophiya-ai <sophiyabr@gmail.com>
2024-10-23 16:39:44 +0200

    collision wrong and dx dy wrong

--- config.py
+++ config.py
@@ -1,14 +1,21 @@
 #константы экрана
 WIDTH = 800
 HEIGHT = 600
-SCREEN_COLOR = "black"
+SCREEN_COLOR = (0,0,0)
+FPS = 60
 
 #КОНСТАНТЫ ИГРОКА - КВАДРАТИКА
 PLAYER_SIZE = 20
 PLAYER_SPEED = 5
 PLAYER_COLOR = "aqua"
+PLAYER_LIFES = 5
 
 #Константы врагов - кружочков
 ENEMY_SIZE = 20
 ENEMY_EMERGE_GAP = 1000
 ENEMY_COLOR = "brown1"
+
+#шрифт
+type = "Arial"
+size = 48
+color = (255,0,0) #красный
--- survival_game.py
+++ survival_game.py
@@ -29,6 +29,78 @@ class Enemy:
     def draw(self,screen):
         pg.draw.circle(screen,conf.ENEMY_COLOR, self.rect.center, conf.ENEMY_SIZE // 2)
 
+#порождаем врагов
+def emerge_enemies():
+    enemies.append(Enemy())
+
+#проверяем столкновение (изображений) и считаем их
+def collision():
+    i = 0
+    for e in enemies:
+        if player.rect.colliderect(e.rect):
+            i += 1
+    return i
+
 pg.init()
 screen = pg.display.set_mode((conf.WIDTH, conf.HEIGHT))
 pg.display.set_caption("Игра на выживание")
+clock = pg.time.Clock() #создаем объект типа "время" вызовем функцию tick()
+                        # с максимальным числом  кадров в секунду
+
+# Инициализация шрифта
+pg.font.init()
+font = pg.font.SysFont(conf.type,conf.size) #создание объекта шрифта с заданным типом и размером
+
+player = Player()
+enemies = []
+emerge_enemy_event = pg.USEREVENT + 1
+pg.time.set_timer(emerge_enemy_event, conf.ENEMY_EMERGE_GAP) #установили таймер для создания события
+                                                            # emerge_enemy_event через заданный интервал
+
+running = True
+while running:
+    screen.fill(conf.SCREEN_COLOR)
+
+    #обработка событий: нажатия крестика и пользовательского события нажатия клавиш
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+        elif event.type == emerge_enemy_event:
+            emerge_enemies()
+
+    # обработка событий: нажатие стрелок
+    keys = pg.key.get_pressed()
+    dx = dy = 0
+    if keys[pg.K_LEFT]:
+        dx -= conf.PLAYER_SPEED
+    if keys[pg.K_RIGHT]:
+        dx = conf.PLAYER_SPEED
+    if keys[pg.K_UP]:
+        dy -= conf.PLAYER_SPEED
+    if keys[pg.K_DOWN]:
+        dx = conf.PLAYER_SPEED
+
+    #запускаем функцию движения из класса Игрок
+    player.move(dx, dy)
+
+    # запускаем функцию проверки столкновений (не более заданного в config числа жизней)
+    check_collision = collision()
+    if check_collision > conf.PLAYER_LIFES:
+        screen.fill(conf.SCREEN_COLOR)    #очищаем экран, залив его цветом
+        text = font.render("Вы потратили все жизни! Игра окончена!", True, conf.color)
+        text_rect = text.get_rect(center = (400,300))
+        screen.blit(text,text_rect) #отрисовка текста на экране
+        pg.time.wait(4000) #задерживаем выход на 2 секунды
+        running = False
+
+    # запускаем функции отрисовки draw из класов игрока и врагов
+    player.draw(screen)
+    for e in enemies:
+        e.draw(screen)
+
+    #обновляем экран
+    pg.display.flip()
+    clock.tick(conf.FPS)
+
+pg.quit()
+
